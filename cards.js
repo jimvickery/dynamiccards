1608c37902e3690f711e734f673870b8e81e6f8d
Jim Vickery <jimrv8@yahoo.com>
2017-07-30 18:50:38 -0500

    added js files

---
+++ cards.js
@@ -0,0 +1,77 @@
+console.log('in cards.js');
+
+
+// Create an HTML page that contains a text area and a button labeled Create.
+// When the user enters in text into the text area and then clicks the create button, 
+// create a new card element in the DOM that includes it's own delete button. You decide the height/width of the card.
+// When the user clicks the Delete button, the containing card, and no other cards, 
+// should then be removed from the DOM. Not just made invisible, actually removed from 
+// the DOM.
+
+//create element placeholders for textarea and buttons
+
+
+//get placeholders
+let createBlogEl = document.getElementById("create-button");
+let textEl = document.getElementById("text-input");
+let blogEl = document.getElementById('new-content');
+let fontEL = document.getElementById("font");
+let deleteBlogsEl = document.getElementById("delete-blogs")
+
+
+blogEl.addEventListener("click", function(){
+ blogEl.innerHTML += `<div id = "new-content" >` + 
+ `<label for = "back"> Select Blog Color</label>
+ <input type = "color id = "back" name = "back" oninput = "changeColor(this);">` +
+ `<label for = "back"> Select Font Color</label>
+ <input type = "color" id = "font" name = "font" oninput = "changeFont(this);">` +
+ `<div class = "blogtext">` + textEl.value + `</p>` + 
+ `<input type = "button" id = " kill " nam e= "kill" value = "delete"  onclick = "kille(this.parentNode);"></div>  ` 
+
+
+
+});
+
+
+//add event listener click event for delet all button
+deleteBlogsEl.addEventListener("click", function(){
+    blogEl.innerHTML = "";
+
+});
+
+function deleteBlogs (a) {
+    let parentBlog = a.parentNode;
+}
+
+function changeColor(a) {
+    let parentBlog = a.parentNode;
+    let childDiv = parent.getElementsByTagName('input') [0];
+    let colors = childDiv.value;
+    parent.getElementsByTagName("p")[0].style.backgroundColor = colors;
+
+
+}
+
+
+function changeFont(a) {
+    let parent = a.parentNode;
+    let childDiv = parent.getElementsByTagName('input')[1];
+    let colors = childDiv.value;
+    parent.style.color = colors;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
